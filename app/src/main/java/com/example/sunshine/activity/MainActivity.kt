c40f326b052feaf69398fb4510df48cf8abff480
Parshva <parshvajain2003@gmail.com>
2022-08-11 01:08:09 +0530

    Login page

--- app/src/main/java/com/example/sunshine/activity/MainActivity.kt
+++ app/src/main/java/com/example/sunshine/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sunshine.activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sunshine.R
 
@@ -8,6 +9,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        if (Build.VERSION.SDK_INT >= Build_VERSION_CODES.R){
+//            window.insetsController?.hide(WindowInsets.Type.statusBars())
+//        }
+//        else{
+//            window.setFlags(
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN
+//            )
+//        }
+        getActionBar()?.hide();
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
 
     }
--- app/src/main/java/com/example/sunshine/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/sunshine/activity/RegisterActivity.kt
@@ -5,14 +5,15 @@ import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sunshine.R
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
         getActionBar()?.hide();
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
     }
 }
